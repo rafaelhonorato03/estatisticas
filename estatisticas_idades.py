9d81bd94542a61a35ade8713090d22e3f4701cd9
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-03-14 20:24:19 -0300

    Update estatisticas_idades.py

--- estatisticas_idades.py
+++ estatisticas_idades.py
@@ -18,9 +18,3 @@ print(f'Número de classes: {k}')
 #Calculo da amplitude de classes
 amplitude_classes = amplitude_idade / k
 print(f'Amplitude de classes: {amplitude_classes}')
-
-amplitude_idade = df['Idade'].max() - df['Idade'].min()
-print(f'Amplitude de idades: {amplitude_idade}')
-##amplitude_vida_feminina = df['Sum of Females  Life Expectancy'].max() - df['Sum of Females  Life Expectancy'].min()
-#amplitude_vida_masculina = df['Sum of Males  Life Expectancy'].max() - df['Sum of Males  Life Expectancy'].min()
-#amplitude_vida_geral = df['Sum of Life Expectancy  (both sexes)'].max() - df['Sum of Life Expectancy  (both sexes)'].min()
