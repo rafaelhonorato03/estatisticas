e729c8ee45f89bc781d6c94e5c2497e6d1d93d3e
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-03-14 08:42:43 -0300

    Update expectativa_de_vida.py

--- expectativa_de_vida.py
+++ expectativa_de_vida.py
@@ -6,11 +6,25 @@ df = pd.read_csv(r'C:\Users\tabat\Documents\GitHub\estatisticas\life_expectancy.
 print(df.head())
 print(df.info())
 
+# Remover as linhas onde a expectativa de vida é maior que 100
+df = df[df['Sum of Life Expectancy  (both sexes)'] <= 100]
+df = df[df['Sum of Females  Life Expectancy'] <= 100]
+df = df[df['Sum of Males  Life Expectancy'] <= 100]
+
+
 # Calcular a amplitude geral da expectativa de vida
 amplitude_vida_feminina = df['Sum of Females  Life Expectancy'].max() - df['Sum of Females  Life Expectancy'].min()
 amplitude_vida_masculina = df['Sum of Males  Life Expectancy'].max() - df['Sum of Males  Life Expectancy'].min()
 amplitude_vida_geral = df['Sum of Life Expectancy  (both sexes)'].max() - df['Sum of Life Expectancy  (both sexes)'].min()
 
+print(df['Sum of Life Expectancy  (both sexes)'].max())
+print(df['Sum of Life Expectancy  (both sexes)'].min())
+
+
 print(f'Amplitude da expectativa de vida feminina: {amplitude_vida_feminina}')
 print(f'Amplitude da expectativa de vida masculina: {amplitude_vida_masculina}')   
 print(f'Amplitude da expectativa de vida geral: {amplitude_vida_geral}')
+
+# Calcular a soma da expectativa de vida quando a expectativa é maior que 100
+soma_expectativa_maior_100 = df[df['Sum of Life Expectancy  (both sexes)'] > 100]
+print(f'Soma da expectativa de vida quando a expectativa é maior que 100: {soma_expectativa_maior_100}')
