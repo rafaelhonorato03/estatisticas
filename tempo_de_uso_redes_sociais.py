aac5d120bf5d47f067f53fe4a62c19af117c0ac5
Rafael Honorato <114627686+rafaelhonorato03@users.noreply.github.com>
2025-03-14 21:07:13 -0300

    Create tempo_de_uso_redes_sociais.py

---
+++ tempo_de_uso_redes_sociais.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#importar o dataset
+df = pd.read_excel(r"C:\Users\tabat\Documents\GitHub\estatisticas\Tempo de uso de redes sociais.xlsx")
+
+print(df.head())
